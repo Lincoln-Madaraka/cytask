6aad76169efbeebc9be1448a3058e3722b9d3af6
Lincoln-Madaraka <madarakalincoln48@gmail.com>
2025-07-14 12:40:47 +0300

    deployment fixes

--- DB_connection.php
+++ DB_connection.php
@@ -1,9 +1,9 @@
 <?php  
 
-$sName = "127.0.0.1";
-$uName = "root";
-$pass  = "";
-$db_name = "task_management_db";
+$sName = getenv('MYSQLHOST') ?: "127.0.0.1";
+$uName = getenv('MYSQLUSER') ?: "root";
+$pass  = getenv('MYSQLPASSWORD') ?: "";
+$db_name = getenv('MYSQLDATABASE') ?: "task_management_db";
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
