2f5ae8d3c198d8a4379488acc34e20755d5fa657
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-08-25 02:42:19 +0300

    deploying

--- DB_connection.php
+++ DB_connection.php
@@ -13,7 +13,6 @@ try {
         $pass
     );
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connection successful!";
 }catch(PDOException $e){
 	echo "Connection failed: ". $e->getMessage();
 	exit;
