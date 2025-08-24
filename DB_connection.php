0c95ae8ecbb64c9d3326a618be59a6e6a3d7ddbc
Lincoln Madaraka <117019629+Lincoln-Madaraka@users.noreply.github.com>
2025-08-25 02:29:44 +0300

    supabase connections

--- DB_connection.php
+++ DB_connection.php
@@ -7,8 +7,13 @@ $db_name = "postgres";
 $port = 6543;
 
 try {
-	$conn = new PDO("pgsql:host=$sName;port=$port;dbname=$db_name", $uName, $pass);
+	$conn = new PDO(
+        "pgsql:host=$sName;port=$port;dbname=$db_name;sslmode=require",
+        $uName,
+        $pass
+    );
 	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection successful!";
 }catch(PDOException $e){
 	echo "Connection failed: ". $e->getMessage();
 	exit;
